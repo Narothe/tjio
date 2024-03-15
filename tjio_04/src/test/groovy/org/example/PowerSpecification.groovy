6e60969f9f947e179859341bf3206f3601012fb8
Narothe <kuba.dojka@gmail.com>
2024-03-15 10:20:33 +0100

    feat: create tjio_04

---
+++ tjio_04/src/test/groovy/org/example/PowerSpecification.groovy
@@ -0,0 +1,16 @@
+package org.example
+
+import spock.lang.Specification
+
+class PowerSpecification extends Specification {
+    def "numbers to the power of two"(int a, int b, int c) {
+        expect:
+            Math.pow(a,b) == c // It is assertion!
+
+        where:
+            a | b | c
+            1 | 2 | 1
+            2 | 2 | 4
+            3 | 2 | 9
+    }
+}
---
+++ tjio_04/src/test/groovy/org/example/SumSpecification.groovy
@@ -0,0 +1,18 @@
+package org.example
+
+import spock.lang.Specification
+
+class SumSpecification extends Specification {
+
+    def "one plus two shoud equal three"() {
+        given:
+            def one = 1
+            def two = 2
+
+        when:
+            def result = one + two
+
+        then:
+            result == 3 // It is assertion!
+    }
+}
