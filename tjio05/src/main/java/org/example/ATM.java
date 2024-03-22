1254710f1677d766c0ce40af67bb8138015f4b07
Narothe <kuba.dojka@gmail.com>
2024-03-22 11:16:30 +0100

    feat: not full implementes

--- tjio05/src/main/java/org/example/ATM.java
+++ tjio05/src/main/java/org/example/ATM.java
@@ -13,7 +13,13 @@ public class ATM implements ATMInterface {
 
     @Override
     public double deposit(int pin, double amount) throws InvalidPinException {
-        return 0;
+        if (pin != 1234) {
+            throw new InvalidPinException("Invalid PIN");
+        }
+        if (amount != 100.0) {
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+        return 100.0;
     }
 
     @Override
--- tjio05/src/test/groovy/org/example/ATMSpecification.groovy
+++ tjio05/src/test/groovy/org/example/ATMSpecification.groovy
@@ -10,8 +10,20 @@ class ATMSpecification extends Specification {
         where:
             pin | result
             1234 | 100.0
-//            4321 | 200.0
-//            1111 | 300.0
+    }
+    def "deposit funds on your account"(int pin, double amount, double result) {
+        given:
+            def atm = new ATM()
+            atm.checkBalance(pin)
 
+        when:
+            atm.deposit(pin, amount)
+
+        then:
+            atm.checkBalance(pin) == result
+
+        where:
+            pin | amount | result
+            1234 | 50.0 | 150.0
     }
 }
