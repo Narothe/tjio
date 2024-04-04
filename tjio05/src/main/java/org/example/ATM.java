bfa23849f7db17a44a6dbc71b61e49f477c798c3
Narothe <kuba.dojka@gmail.com>
2024-04-04 22:42:06 +0200

    feat, fix: lab5: checkBalance, deposit, withdraw

--- tjio05/src/main/java/org/example/ATM.java
+++ tjio05/src/main/java/org/example/ATM.java
@@ -1,29 +1,52 @@
 package org.example;
 
 public class ATM implements ATMInterface {
+    private double accountBalance;
 
+    public ATM(double initialBalance) {
+        this.accountBalance = initialBalance;
+    }
 
     @Override
     public double checkBalance(int pin) throws InvalidPinException {
+        System.out.println("\ncheckBalance:");
         if (pin != 1234) {
             throw new InvalidPinException("Invalid PIN");
         }
-        return 100.0;
+        System.out.println("Checking balance: " + accountBalance);
+        return accountBalance;
     }
 
     @Override
     public double deposit(int pin, double amount) throws InvalidPinException {
+        System.out.println("\ndeposit:");
         if (pin != 1234) {
             throw new InvalidPinException("Invalid PIN");
         }
-        if (amount != 100.0) {
+        if (amount <= 0.0) {
             throw new IllegalArgumentException("Amount must be positive");
         }
-        return 100.0;
+        accountBalance += amount;
+        System.out.println("Adding amount: " + amount);
+        System.out.println("Current balance: " + accountBalance);
+        return accountBalance;
     }
 
     @Override
     public double withdraw(int pin, double amount) throws InsufficientFundsException, InvalidPinException {
-        return 0;
+        System.out.println("\nwithdraw:");
+        if (pin != 1234) {
+            throw new InvalidPinException("Invalid PIN");
+        }
+        if (amount <= 0.0) {
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+        if (amount > 100.0) {
+            throw new InsufficientFundsException("Insufficient funds");
+        }
+        accountBalance -= amount;
+        System.out.println("Draw amount: " + amount);
+        System.out.println("Current balance: " + accountBalance);
+        return accountBalance;
     }
 }
--- tjio05/src/test/groovy/org/example/ATMSpecification.groovy
+++ tjio05/src/test/groovy/org/example/ATMSpecification.groovy
@@ -3,17 +3,22 @@ package org.example
 import spock.lang.Specification
 
 class ATMSpecification extends Specification {
-    def "check balance"(int pin, double result) {
+    def "check balance"(int pin, double initialBalance, double result) {
+        println "Running 'check balance' test"
+        given:
+        def atm = new ATM(initialBalance)
+
         expect:
-            new ATM().checkBalance(pin) == result
+        atm.checkBalance(pin) == result
 
         where:
-            pin | result
-            1234 | 100.0
+        pin | initialBalance | result
+        1234 | 500.0 | 500.0
     }
-    def "deposit funds on your account"(int pin, double amount, double result) {
+    def "deposit funds on your account"(int pin, double amount, double initialBalance, double result) {
+        println "\n\nRunning 'deposit funds on your account' test"
         given:
-            def atm = new ATM()
+        def atm = new ATM(initialBalance)
             atm.checkBalance(pin)
 
         when:
@@ -23,7 +28,23 @@ class ATMSpecification extends Specification {
             atm.checkBalance(pin) == result
 
         where:
-            pin | amount | result
-            1234 | 50.0 | 150.0
+            pin | amount | initialBalance | result
+            1234 | 100.0 | 500.0 | 600.0
+    }
+    def "withdraw funds from your account"(int pin, double amount, double initialBalance, double result) {
+        println "\n\nRunning 'withdraw funds from your account' test"
+        given:
+            def atm = new ATM(initialBalance)
+            atm.checkBalance(pin)
+
+        when:
+            atm.withdraw(pin, amount)
+
+        then:
+            atm.checkBalance(pin) == result
+
+        where:
+            pin | amount | initialBalance | result
+            1234 | 100.0 | 500.0 | 400.0
     }
 }
